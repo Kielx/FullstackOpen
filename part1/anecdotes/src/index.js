c3e3653c5844eb2feb2a0f6d618db142249c9c15
Kielx <kiel66@gmail.com>
2020-09-10 19:34:10 +0200

    Anecdotes 1.13 done

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,26 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import Button from "./components/Button";
-
-const App = (props) => {
-  const [selected, setSelected] = useState(0);
-
-  const handleNextAnecdoteClick = () => {
-    let rand = Math.floor(Math.random() * anecdotes.length);
-    console.log(rand, selected);
-    while (rand === selected) {
-      rand = Math.floor(Math.random() * anecdotes.length);
-    }
-    return setSelected(rand);
-  };
-
-  return (
-    <>
-      <p>{props.anecdotes[selected]}</p>
-      <Button handleNextAnecdoteClick={handleNextAnecdoteClick}></Button>
-    </>
-  );
-};
+import NextButton from "./components/NextButton";
+import VoteButton from "./components/VoteButton";
 
 const anecdotes = [
   "If it hurts, do it more often",
@@ -30,5 +11,41 @@ const anecdotes = [
   "Premature optimization is the root of all evil.",
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 ];
+
+//create object for points filled with zeros
+const myObj = {};
+for (let i = 0; i < anecdotes.length; i++) {
+  myObj[i] = 0;
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(myObj);
+
+  const handleNextAnecdoteClick = () => {
+    let rand = Math.floor(Math.random() * anecdotes.length);
+    while (rand === selected) {
+      rand = Math.floor(Math.random() * anecdotes.length);
+    }
+    return setSelected(rand);
+  };
+
+  const handleVote = () => {
+    const copy = { ...points };
+    copy[selected] += 1;
+    setPoints({ ...copy });
+  };
+
+  return (
+    <>
+      <p>{props.anecdotes[selected]}</p>
+      <NextButton
+        handleNextAnecdoteClick={handleNextAnecdoteClick}
+      ></NextButton>
+      <VoteButton handleVote={handleVote} points={points}></VoteButton>
+      <p>has {points[selected]} votes</p>
+    </>
+  );
+};
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
