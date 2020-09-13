9c5b51ff4b3aa83ab74f83de50e8dc03d28f7747
Kielx <kiel66@gmail.com>
2020-09-13 10:33:21 +0200

    Show all languages spoken in selected country

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const CountryDetails = ({ country }) => {
+  let languages = country.languages.map((language) => (
+    <li key={language.name}>{language.name}</li>
+  ));
   return (
     <>
       <h2>{country.name}</h2>
       <p>{`Capital: ${country.capital}`}</p>
       <p>{`Population: ${country.population}`}</p>
-      <p>{`Language: ${country.languages[0].name}`}</p>
+      <h3>Languages:</h3>
+      <ol>{languages}</ol>
       <img
         src={country.flag}
         alt="Country Flag"
